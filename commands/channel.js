b2cc9f13fe6d094c1047459c7052376540366997
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-09-09 16:35:35 +0700

    gabut

--- commands/channel.js
+++ commands/channel.js
@@ -1,10 +1,13 @@
+const Discord = require("discord.js-selfbot");
+
 module.exports.run = async(client, msg, args) => {
   let guild = client.guilds.cache.get(args[0]) || msg.guild;
   if (!guild) return console.log("Make sure you run this command on server");
   let channelList = [];
+  let embed = new Discord.MessageEmbed();
   let list = guild.channels.cache.filter(x => x.type == "voice");
   list.map(x => {
     channelList.push(`${x} ・ ${x.id}`);
   });
-  return msg.channel.send(`>>> **LIST VOICE CHANNEL SERVER ${guild.name}**\n\n`+channelList.map((x, i) => `**${i+1}**. ${x}`).join("\n"));
+  return msg.channel.send(embed.setTitle(`LIST VOICE CHANNEL SERVER ${guild.name}`).setDescription(">>> "+channelList.map((x, i) => `**${i+1}**. ${x}`).join("\n")).setColor("RANDOM"));
 };
