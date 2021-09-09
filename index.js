98a4c327a8adf1dfcdaa5af449138256c594a227
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-09-09 11:30:34 +0700

    Add {prefix}spam 5 anjay mabar

--- index.js
+++ index.js
@@ -50,6 +50,25 @@ client.on("message", async (msg) => {
     return console.log("Keluar dari voice channel");
   }
   
+  /*
+    {prefix}spam [time (berapa kali user akan mengirim pesan)] [message]
+  */
+  if(cmd == "spam") {
+    let times = args[0];
+    if(!times) return console.log("Parameter time must be filled");
+    if(isNaN(times)) return console.log("Invalid number");
+    
+    let spamMessage = args.slice(1).join(" ");
+    if(!spamMessage) return console.log("Parameter message must be fille");
+    if(spamMessage.length > 1900) return console.log("Limit");
+    
+    // Spam akan berhenti jika hosting mati.
+    // Spam tidak bisa di berhentikan sampai angka tertentu
+    for(let i = 0; i < times; i++) {
+      msg.channel.send(spamMessage);
+    }
+  }
+  
 });
-
+console.log(process.env.TOKEN)
 client.login(process.env.TOKEN);
