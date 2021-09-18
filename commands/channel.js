95ce2447acd251bc0ac7e2c83d54135a0960af45
jeremy776 <jeremy.kusuma2018@gmail.com>
2021-09-18 20:26:24 +0700

    ok

--- commands/channel.js
+++ commands/channel.js
@@ -3,34 +3,34 @@ const numberEmote = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣"];
 
 module.exports.run = async(client, msg, args) => {
   let idServer = args[0];
-  if(!idServer) return console.log("Please put id server");
+  if (!idServer) return console.log("Please put id server");
   let guild = client.guilds.cache.get(idServer);
-  if(!guild) return console.log("Server Not Found");
-  
+  if (!guild) return console.log("Server Not Found");
+
   let pages = args[1];
-  if(!pages) pages = 1;
-  if(isNaN(pages)) return console.log("invalid number");
+  if (!pages) pages = 1;
+  if (isNaN(pages)) return console.log("invalid number");
   let page = pages - 1;
-  
+
   let current = page * 5;
-  
+
   let channelList = [];
   let embed = new Discord.MessageEmbed();
   let list = guild.channels.cache.filter(x => x.type == "voice")
-             .map(x => x.rawPosition)
-             .sort((a, b) => a - b);
+  .map(x => x.rawPosition)
+  .sort((a, b) => a - b);
   list.map(x => {
     let channelFilter = guild.channels.cache.filter(g => g.type == "voice")
     .find(g => g.rawPosition == x);
     channelList.push(`${channelFilter} ・ ${channelFilter.id}`);
   });
-  
+
   msg.channel.send(buatEmbed(page, current, channelList, guild))
 };
 
 function buatEmbed(page, jumlah, channelList, guild) {
   let current = channelList.slice(jumlah, jumlah + 5);
-  if(current.length == 0) {
+  if (current.length == 0) {
     return new Discord.MessageEmbed()
     .setColor("RED")
     .setDescription("**Not Found**");
@@ -39,12 +39,13 @@ function buatEmbed(page, jumlah, channelList, guild) {
   let totalPage = channelList.length / 5;
   let toArrayPage = totalPage.toString().split(".");
   let newTotalPage;
-  
-  if(toArrayPage[1].length !== 0) {
-    totalPage = Number(toArrayPage[0]) + 1;
-    
+
+  if (toArrayPage[1]) {
+    if (toArrayPage[1].length !== 0) {
+      totalPage = Number(toArrayPage[0]) + 1;
+    }
   }
-  
+
   let embed = new Discord.MessageEmbed()
   .setTitle("Channel List in "+ guild.name)
   .setColor("RANDOM")
